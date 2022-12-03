a61acc2ba8c975f88f61d7309d56fa019cc20741
Alya <abogdan699@gmail.com>
2022-12-03 13:38:48 +0100

    Init base components

---
+++ src/components/PokemonMain.tsx
@@ -0,0 +1,8 @@
+import PokemonTable from "./PokemonTable";
+import React from "react";
+
+export default function PokemonMain() {
+    return (
+        <PokemonTable />
+    )
+}
---
+++ src/components/PokemonTable.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+export default function PokemonTable() {
+  return <Box>Test</Box>;
+}
